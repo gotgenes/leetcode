e2d84b7009caf138ba036c6e1900b515ba4fbeb4
Chris Lasher <chris.lasher@gmail.com>
2024-02-15 09:33:50 -0500

    Initial commit.

---
+++ 20.go
@@ -0,0 +1,30 @@
+package leetcode
+
+func isValid(s string) bool {
+	pairs := make([]rune, 0)
+	for _, c := range s {
+		switch c {
+		case ')':
+			if len(pairs) == 0 || pairs[len(pairs)-1] != '(' {
+				return false
+			}
+			pairs = pairs[:len(pairs)-1]
+		case ']':
+			if len(pairs) == 0 || pairs[len(pairs)-1] != '[' {
+				return false
+			}
+			pairs = pairs[:len(pairs)-1]
+		case '}':
+			if len(pairs) == 0 || pairs[len(pairs)-1] != '{' {
+				return false
+			}
+			pairs = pairs[:len(pairs)-1]
+		default:
+			pairs = append(pairs, c)
+		}
+	}
+	if len(pairs) > 0 {
+		return false
+	}
+	return true
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gotgenes/leetcode
+
+go 1.21.7
