dba17b0e419b62c5e16e36f455d040a96a2dc28d
Chris Lasher <chris.lasher@gmail.com>
2024-02-19 16:02:29 -0500

    Add solution for sandwiches queue.

---
+++ 1700-number_of_students_unable_to_eat_lunch.go
@@ -0,0 +1,25 @@
+func countStudents(students []int, sandwiches []int) int {
+	for _, sandwich := range sandwiches {
+		for i, studentPref := range students {
+			if studentPref == sandwich {
+				if i == 0 {
+					if len(students) > 1 {
+						students = students[1:]
+					} else {
+						students = make([]int, 0)
+					}
+				} else if i == len(students)-1 {
+					students = students[:len(students)-1]
+				} else {
+					students = append(students[i+1:], students[:i]...)
+				}
+				break
+			} else {
+                if i == len(students)-1 {
+                    return len(students)
+                }
+            }
+		}
+	}
+    return 0
+}
